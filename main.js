d46f4c0b4646bab74391a78986fd2b326bd86834
mahenooransari93 <47055845+mahenooransari93@users.noreply.github.com>
2019-01-26 16:58:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.write("<table  width='200'>")
+document.write("<tr><td></td><td><img src='image.png' alt='hello' width='500' height='500'</img></td></tr>");
+document.write("<tr><td>XYZ </td><td></td></tr>");
+document.write("<tr><td>XYZ </td><td></td></tr>");
+document.write("<tr><td>123 </td><td></td></tr>");
+document.write("<tr><td>XYZ </td><td></td></tr>");
+document.write("<tr><td>XYZ </td><td></td></tr>");
+document.write("<tr><td>XYZ </td><td></td></tr>");
+
+
+document.write("</table>")
---
+++ main1.js
@@ -0,0 +1,9 @@
+document.write("<div>")
+document.write("<span>XYZ</span><br/>");
+document.write("<span>XYZ</span><br/>");
+document.write("<span>123</span><br/>");
+document.write("<span>XYZ</span><br/>");
+document.write("<span>XYZ</span><br/>");
+document.write("<span>XYZ</span><br/>");
+document.write("<span><img src='image.png' alt='hello' width='100' height='150'</img></sp>");
+document.write("</div>")
